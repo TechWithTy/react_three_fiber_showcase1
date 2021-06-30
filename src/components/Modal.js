922dc9e84af08066decf600c080db06a3934ef9a
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-29 22:34:11 -0600

    Contact Form Working

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Modal from 'react-modal';
-import { nameValidation } from './FormValidation';
+import {
+  emailValidation,
+  messageValidation,
+  nameValidation,
+  phoneNumberValidation,
+} from './FormValidation';
 const customStyles = {
   content: {
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
-        marginRight: '-50%',
+    marginRight: '-50%',
     width: '30%',
     transform: 'translate(-50%, -50%)',
     zIndex: '99999999999999999999999999',
@@ -22,12 +27,20 @@ Modal.setAppElement('#root');
 function ContactModal({ modalOpen, setModalState }) {
   const { register, handleSubmit } = useForm();
   const [nameError, setNameError] = useState(false);
+  const [phoneError, setphoneError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
   }
 
   const onSubmit = async (data) => {
-    setNameError(nameValidation(data.firstName))
+    setNameError(nameValidation(data.firstName));
+    setphoneError(phoneNumberValidation(data.phoneNumber));
+    setEmailError(emailValidation(data.email));
+    setMessageError(messageValidation(data.message));
+
     console.log(data.firstName.length, data.firstName);
   };
 
@@ -59,13 +72,26 @@ function ContactModal({ modalOpen, setModalState }) {
                   <form onSubmit={handleSubmit(onSubmit)}>
                     <label>First And Last Name</label>
                     <input {...register('firstName')} />
-                    {nameError && (
-                      <p className="error">
-                        {nameError}
-                      </p>
-                    )}
-                    <label>Laste Name</label>
-                    <input {...register('lastName')} />
+                    {nameError && <p className="error">{nameError}</p>}
+                    <label>Phone Number</label>
+                    <input type="number" {...register('phoneNumber')} />
+                    {phoneError && <p className="error">{phoneError}</p>}
+                    <label>Email Address</label>
+                    <input type="email" {...register('email')} />
+                    {emailError && <p className="error">{emailError}</p>}
+                    <label>Message</label>
+
+                    <textarea
+                      rows="5"
+                      cols="60"
+                      name="description"
+                      {...register('message')}
+                    >
+                      Enter details here.../
+                    </textarea>
+                    {messageError && <p className="error">{messageError}</p>}
+
+                    <br></br>
                     <input type="submit" />
                   </form>
                 </div>
